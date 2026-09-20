cf2fa44dac3ad94d4ffbe6ef36e37d73aeb9ae2f
winni805 <69179493+winni805@users.noreply.github.com>
2026-09-20 18:00:29 +0800

    Update H5/PWA c745db44cf90

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'xigai-h5-f85c8c150a9d';
+const CACHE = 'xigai-h5-c745db44cf90';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './data.js',
   './manifest.webmanifest', './assets/icon.svg', './assets/icon-192.png', './assets/icon-512.png'
